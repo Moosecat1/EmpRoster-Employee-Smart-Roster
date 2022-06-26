7597aba96fe2cc7e51dbff905b114f149260e725
adam <adam_muscat@yahoo.com>
2022-06-26 15:20:10 +1000

    cringe

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import '../css/Home.css';
+import React from "react";
+import Navbar from "./navbar";
+import Footer from "./footer";
+
+
+export default function Home() {
+
+    return (
+        <main>
+            <Navbar/>
+
+            Home
+
+            <Footer/>
+        </main>
+
+
+    )
+}
