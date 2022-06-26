0e0b0b80350b8c5695f7798535d2f6149699e4d3
helloworldgaming <apdl797@uowmail.edu.au>
2022-06-26 18:02:36 +1000

    create + drop

--- src/db/create.sql
+++ src/db/create.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS ROSTER;
-USE ROSTER;
+CREATE DATABASE IF NOT EXISTS SMARTROSTER;
+USE SMARTROSTER;
 
 CREATE TABLE Company(
     company_id INT NOT NULL AUTO_INCREMENT,
@@ -17,14 +17,33 @@ CREATE TABLE OperatingTime(
 );
 
 CREATE TABLE Employee(
-    emp_id VARCHAR(255),
+    emp_id INT NOT NULL AUTO_INCREMENT,
     emp_fName VARCHAR(255),
     emp_lName VARCHAR(255),
     emp_email VARCHAR(255),
     emp_phNum VARCHAR(255),
     emp_type VARCHAR(50),
+    emp_privilege VARCHAR(50),
     company_id INT,
     PRIMARY KEY(emp_id),
     FOREIGN KEY(company_id) REFERENCES Company(company_id),
-    CONSTRAINT types CHECK(emp_type="Casual" OR emp_type="Part-time" OR emp_type="Full-time")
+    CONSTRAINT types CHECK(emp_type="Casual" OR emp_type="Part-time" OR emp_type="Full-time"),
+    CONSTRAINT privileges CHECK(emp_privilege="Employee" OR emp_privilege="Manager" OR emp_privilege="Admin")
+);
+
+CREATE TABLE Availability(
+    avail_date DATE,
+    avail_start TIME,
+    avail_end TIME,
+    emp_id INT,
+    FOREIGN KEY(emp_id) REFERENCES Employee(emp_id)
+);
+
+CREATE TABLE Roster(
+    rost_date DATE,
+    rost_start TIME,
+    rost_end TIME,
+    rost_week_start DATE,
+    emp_id INT,
+    FOREIGN KEY(emp_id) REFERENCES Employee(emp_id)
 );
