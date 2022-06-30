bdb879c5702ca2eb85b7f62383df7b2fdc56d383
waffledoffle <jackwaffledoffle@gmail.com>
2022-06-30 20:39:58 +1000

    how do i use this help

---
+++ src/db/insert.sql
@@ -0,0 +1,19 @@
+USE SMARTROSTER;
+
+INSERT INTO Company
+VALUES ('1', 'Joes MegaCorp');
+
+INSERT INTO OperatingTime
+VALUES ('Monday', '9:00', '17:00', '1');
+INSERT INTO OperatingTime
+VALUES ('Tuesday', '9:00', '17:00', '1');
+INSERT INTO OperatingTime
+VALUES ('Wednesday', '9:00', '17:00', '1');
+INSERT INTO OperatingTime
+VALUES ('Thursday', '9:00', '17:00', '1');
+INSERT INTO OperatingTime
+VALUES ('Friday', '9:00', '17:00', '1');
+INSERT INTO OperatingTime
+VALUES ('Saturday', '9:00', '17:00', '1');
+INSERT INTO OperatingTime
+VALUES ('Sunday', '9:00', '17:00', '1');
