d1c371c34c4f5f3714c7648156106ebcd6cf9a87
waffledoffle <jackwaffledoffle@gmail.com>
2022-07-04 21:31:53 +1000

    Created roster.jsx and roster.css

---
+++ src/components/roster.jsx
@@ -0,0 +1,233 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import ListGroup from 'react-bootstrap/ListGroup';
+import '../css/roster.css';
+
+function Roster() {
+
+    /*Need to add functionality for generating roster from DB*/
+    return (
+        <div className="d-flex flex-nowrap roster-container">
+            <ListGroup>
+                <ListGroup.Item variant="secondary" className="empty"></ListGroup.Item>
+                <ListGroup.Item variant="secondary">0:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">1:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">2:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">3:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">4:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">5:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">6:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">7:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">8:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">9:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">10:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">11:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">12:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">13:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">14:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">15:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">16:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">17:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">18:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">19:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">20:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">21:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">22:00</ListGroup.Item>
+                <ListGroup.Item variant="secondary">23:00</ListGroup.Item>
+            </ListGroup>
+            <ListGroup>
+                <ListGroup.Item variant="secondary">Monday</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+                <ListGroup.Item variant="success">Rostered</ListGroup.Item>
+            </ListGroup>
+            <ListGroup>
+                <ListGroup.Item variant="secondary">Tuesday</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+                <ListGroup.Item variant="danger">Not Rostered</ListGroup.Item>
+            </ListGroup>
+            <ListGroup>
+                <ListGroup.Item variant="secondary">Wednesday</ListGroup.Item>
+                <ListGroup.Item>0:00</ListGroup.Item>
+                <ListGroup.Item>1:00</ListGroup.Item>
+                <ListGroup.Item>2:00</ListGroup.Item>
+                <ListGroup.Item>3:00</ListGroup.Item>
+                <ListGroup.Item>4:00</ListGroup.Item>
+                <ListGroup.Item>5:00</ListGroup.Item>
+                <ListGroup.Item>6:00</ListGroup.Item>
+                <ListGroup.Item>7:00</ListGroup.Item>
+                <ListGroup.Item>8:00</ListGroup.Item>
+                <ListGroup.Item>9:00</ListGroup.Item>
+                <ListGroup.Item>10:00</ListGroup.Item>
+                <ListGroup.Item>11:00</ListGroup.Item>
+                <ListGroup.Item>12:00</ListGroup.Item>
+                <ListGroup.Item>13:00</ListGroup.Item>
+                <ListGroup.Item>14:00</ListGroup.Item>
+                <ListGroup.Item>15:00</ListGroup.Item>
+                <ListGroup.Item>16:00</ListGroup.Item>
+                <ListGroup.Item>17:00</ListGroup.Item>
+                <ListGroup.Item>18:00</ListGroup.Item>
+                <ListGroup.Item>19:00</ListGroup.Item>
+                <ListGroup.Item>20:00</ListGroup.Item>
+                <ListGroup.Item>21:00</ListGroup.Item>
+                <ListGroup.Item>22:00</ListGroup.Item>
+                <ListGroup.Item>23:00</ListGroup.Item>
+            </ListGroup>
+            <ListGroup>
+                <ListGroup.Item variant="secondary">Thursday</ListGroup.Item>
+                <ListGroup.Item>0:00</ListGroup.Item>
+                <ListGroup.Item>1:00</ListGroup.Item>
+                <ListGroup.Item>2:00</ListGroup.Item>
+                <ListGroup.Item>3:00</ListGroup.Item>
+                <ListGroup.Item>4:00</ListGroup.Item>
+                <ListGroup.Item>5:00</ListGroup.Item>
+                <ListGroup.Item>6:00</ListGroup.Item>
+                <ListGroup.Item>7:00</ListGroup.Item>
+                <ListGroup.Item>8:00</ListGroup.Item>
+                <ListGroup.Item>9:00</ListGroup.Item>
+                <ListGroup.Item>10:00</ListGroup.Item>
+                <ListGroup.Item>11:00</ListGroup.Item>
+                <ListGroup.Item>12:00</ListGroup.Item>
+                <ListGroup.Item>13:00</ListGroup.Item>
+                <ListGroup.Item>14:00</ListGroup.Item>
+                <ListGroup.Item>15:00</ListGroup.Item>
+                <ListGroup.Item>16:00</ListGroup.Item>
+                <ListGroup.Item>17:00</ListGroup.Item>
+                <ListGroup.Item>18:00</ListGroup.Item>
+                <ListGroup.Item>19:00</ListGroup.Item>
+                <ListGroup.Item>20:00</ListGroup.Item>
+                <ListGroup.Item>21:00</ListGroup.Item>
+                <ListGroup.Item>22:00</ListGroup.Item>
+                <ListGroup.Item>23:00</ListGroup.Item>
+            </ListGroup>
+            <ListGroup>
+                <ListGroup.Item variant="secondary">Friday</ListGroup.Item>
+                <ListGroup.Item>0:00</ListGroup.Item>
+                <ListGroup.Item>1:00</ListGroup.Item>
+                <ListGroup.Item>2:00</ListGroup.Item>
+                <ListGroup.Item>3:00</ListGroup.Item>
+                <ListGroup.Item>4:00</ListGroup.Item>
+                <ListGroup.Item>5:00</ListGroup.Item>
+                <ListGroup.Item>6:00</ListGroup.Item>
+                <ListGroup.Item>7:00</ListGroup.Item>
+                <ListGroup.Item>8:00</ListGroup.Item>
+                <ListGroup.Item>9:00</ListGroup.Item>
+                <ListGroup.Item>10:00</ListGroup.Item>
+                <ListGroup.Item>11:00</ListGroup.Item>
+                <ListGroup.Item>12:00</ListGroup.Item>
+                <ListGroup.Item>13:00</ListGroup.Item>
+                <ListGroup.Item>14:00</ListGroup.Item>
+                <ListGroup.Item>15:00</ListGroup.Item>
+                <ListGroup.Item>16:00</ListGroup.Item>
+                <ListGroup.Item>17:00</ListGroup.Item>
+                <ListGroup.Item>18:00</ListGroup.Item>
+                <ListGroup.Item>19:00</ListGroup.Item>
+                <ListGroup.Item>20:00</ListGroup.Item>
+                <ListGroup.Item>21:00</ListGroup.Item>
+                <ListGroup.Item>22:00</ListGroup.Item>
+                <ListGroup.Item>23:00</ListGroup.Item>
+            </ListGroup>
+            <ListGroup>
+                <ListGroup.Item variant="secondary">Saturday</ListGroup.Item>
+                <ListGroup.Item>0:00</ListGroup.Item>
+                <ListGroup.Item>1:00</ListGroup.Item>
+                <ListGroup.Item>2:00</ListGroup.Item>
+                <ListGroup.Item>3:00</ListGroup.Item>
+                <ListGroup.Item>4:00</ListGroup.Item>
+                <ListGroup.Item>5:00</ListGroup.Item>
+                <ListGroup.Item>6:00</ListGroup.Item>
+                <ListGroup.Item>7:00</ListGroup.Item>
+                <ListGroup.Item>8:00</ListGroup.Item>
+                <ListGroup.Item>9:00</ListGroup.Item>
+                <ListGroup.Item>10:00</ListGroup.Item>
+                <ListGroup.Item>11:00</ListGroup.Item>
+                <ListGroup.Item>12:00</ListGroup.Item>
+                <ListGroup.Item>13:00</ListGroup.Item>
+                <ListGroup.Item>14:00</ListGroup.Item>
+                <ListGroup.Item>15:00</ListGroup.Item>
+                <ListGroup.Item>16:00</ListGroup.Item>
+                <ListGroup.Item>17:00</ListGroup.Item>
+                <ListGroup.Item>18:00</ListGroup.Item>
+                <ListGroup.Item>19:00</ListGroup.Item>
+                <ListGroup.Item>20:00</ListGroup.Item>
+                <ListGroup.Item>21:00</ListGroup.Item>
+                <ListGroup.Item>22:00</ListGroup.Item>
+                <ListGroup.Item>23:00</ListGroup.Item>
+            </ListGroup>
+            <ListGroup>
+                <ListGroup.Item variant="secondary">Sunday</ListGroup.Item>
+                <ListGroup.Item>0:00</ListGroup.Item>
+                <ListGroup.Item>1:00</ListGroup.Item>
+                <ListGroup.Item>2:00</ListGroup.Item>
+                <ListGroup.Item>3:00</ListGroup.Item>
+                <ListGroup.Item>4:00</ListGroup.Item>
+                <ListGroup.Item>5:00</ListGroup.Item>
+                <ListGroup.Item>6:00</ListGroup.Item>
+                <ListGroup.Item>7:00</ListGroup.Item>
+                <ListGroup.Item>8:00</ListGroup.Item>
+                <ListGroup.Item>9:00</ListGroup.Item>
+                <ListGroup.Item>10:00</ListGroup.Item>
+                <ListGroup.Item>11:00</ListGroup.Item>
+                <ListGroup.Item>12:00</ListGroup.Item>
+                <ListGroup.Item>13:00</ListGroup.Item>
+                <ListGroup.Item>14:00</ListGroup.Item>
+                <ListGroup.Item>15:00</ListGroup.Item>
+                <ListGroup.Item>16:00</ListGroup.Item>
+                <ListGroup.Item>17:00</ListGroup.Item>
+                <ListGroup.Item>18:00</ListGroup.Item>
+                <ListGroup.Item>19:00</ListGroup.Item>
+                <ListGroup.Item>20:00</ListGroup.Item>
+                <ListGroup.Item>21:00</ListGroup.Item>
+                <ListGroup.Item>22:00</ListGroup.Item>
+                <ListGroup.Item>23:00</ListGroup.Item>
+            </ListGroup>
+        </div>
+    )
+
+
+
+}
+export default Roster;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,9 @@ import React from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import Sidebar from "../components/sidebarManager";
+import Roster from "../components/roster"
 
 export default function Home() {
     document.title = "EmpRoster - Smart Roster System"
-    return (<main><Navbar/> <Footer/> <Sidebar/></main>);
+    return (<main><Navbar/> <Footer/> <Sidebar/> <Roster/></main>);
 }
