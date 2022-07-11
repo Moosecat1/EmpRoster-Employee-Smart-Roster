7495610057a64bdf5a86ed1367d27dce099cd264
waffledoffle <jackwaffledoffle@gmail.com>
2022-07-11 22:15:35 +1000

    created EmployeeList.css

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,5 +1,6 @@
 import Sidebar from "../components/sidebarManager";
 import Navbar from "../components/navbar";
+import '../css/EmployeeList.css';
 import React from "react";
 
 const axios = require('axios');
@@ -15,9 +16,40 @@ export default function EmployeeList() {
             await axios.get("http://localhost:2420/getEmployeesList/" + 1).then((response) => {
                 console.log(response.data);
                 employeeList = response.data;
+                const container = document.getElementById("eListContainer");
+                const pill = document.createElement("ul");
+                pill.className = "eList-pill flex-column flex-nowrap mb-auto";
                 for (let i = 0; i < employeeList.length; i++) {
-                    document.getElementById("eListContainer").innerHTML += employeeList[i].emp_fName;
+                    const listItem = document.createElement("li");
+                    listItem.className = "eList-item flex-row mb-auto";
+                    const listLink = document.createElement("a");
+                    listLink.className = "eList-link flex-column";
+                    listLink.href = "#";
+                    const employeePicture = document.createElement("img");
+                    employeePicture.src = "placeholder.jpg";
+                    employeePicture.src = "Placeholder";
+                    employeePicture.className = "eList-image";
+                    listLink.append(employeePicture);
+                    const employeeName = document.createElement("p");
+                    employeeName.className = "eList-name text-black";
+                    employeeName.innerHTML = employeeList[i].emp_fName + " " + employeeList[i].emp_lName;
+                    listLink.append(employeeName);
+                    const employeeRole = document.createElement("p");
+                    employeeRole.className = "eList-role text-black";
+                    employeeRole.innerHTML = "Placeholder role";
+                    listLink.append(employeeRole);
+                    const employeeType = document.createElement("p");
+                    employeeType.className = "eList-type text-black";
+                    employeeType.innerHTML = employeeList[i].emp_type;
+                    listLink.append(employeeType);
+                    const viewEmployee = document.createElement("p");
+                    viewEmployee.className = "eList-view text-black";
+                    viewEmployee.innerHTML = "View Employee";
+                    listLink.append(viewEmployee);
+                    listItem.append(listLink);
+                    pill.append(listItem);
                 }
+                container.append(pill);
             });
         })();
     }
@@ -27,12 +59,11 @@ export default function EmployeeList() {
     }
 
     return (
-        <div>
+        <div className='flex'>
             <Navbar/> <Sidebar/>
-            <div className='d-flex flex-nowrap justify-content-center' id="eListContainer">
+            <container className='d-flex flex-nowrap justify-content-center' id="eListContainer">
 
-                hello
-            </div>
+            </container>
         </div>
 
 
