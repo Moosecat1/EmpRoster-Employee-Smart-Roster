63854d529eb522b9b415aad070bfde432b57652f
helloworldgaming <apdl797@uowmail.edu.au>
2022-08-01 19:41:35 +1000

    hero import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import Sidebar from "../components/sidebar";
 import Roster from "../components/roster"
+import Hero from "../components/hero"
 
 export default function Home() {
     document.title = "EmpRoster - Smart Roster System"
